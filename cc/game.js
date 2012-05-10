fa056a2b175ebc93ff47a005c8aa93992f5c577f
Maria-Ines Carrera <mariaines.carrera@gmail.com>
2012-05-10 15:17:52 -0700

    eof newlines and robot bugfix

--- cc/game.js
+++ cc/game.js
@@ -9,11 +9,8 @@ goog.require('cc.World');
 cc.Game = function(mode) {
   goog.base(this);
 
-  this.WIDTH = 800;
-  this.HEIGHT = 500;
-
   this.setAnchorPoint(0,0);
-  this.world = new cc.World(0).setSize(this.WIDTH,this.HEIGHT);
+  this.world = new cc.World(0).setSize(cc.Game.WIDTH,cc.Game.HEIGHT);
   this.appendChild(this.world);
   
   this.robot = new cc.Robot().setPosition(10,350);
