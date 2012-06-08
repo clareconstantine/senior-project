008ee6a0381edc516022da6efa3bb461e7732083
Maria-Ines Carrera <mariaines.carrera@gmail.com>
2012-06-07 19:35:05 -0700

    Oops left out coin.jsgit add coin.jsgit add coin.js

---
+++ cc/coin.js
@@ -0,0 +1,16 @@
+goog.provide('cc.Coin');
+
+goog.require('lime.Circle');
+
+cc.Coin = function() {
+  goog.base(this);
+  this.setSize(30,30).setFill('#FFC125').setAnchorPoint(0,0);
+  this.coinGrabbed = false;
+};
+goog.inherits(cc.Coin, lime.Circle);
+
+
+cc.Coin.prototype.robotCollided = function() {
+  this.setHidden(true);
+  this.coinGrabbed = true;
+};
